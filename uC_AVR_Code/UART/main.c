5ef4d33f8cffa0ecfe5c6622eabb9a39a92c6b5f
YiyoChimal <165099057+YiyoChimal@users.noreply.github.com>
2024-04-01 19:53:47 -0600

    Update main.c

--- uC_AVR_Code/UART/main.c
+++ uC_AVR_Code/UART/main.c
@@ -11,20 +11,26 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
-#include <string.h>
+#include <string.h> //Cadena de caracteres; 
+#include <stdio.h>
 
 char ComandoStr[20];
 volatile unsigned char RxContador=0;
 char rx='\0';
-
+unsigned char numero=12; 
 
 int main(void)
 {
 	cli();
+	DDRB|= (1<<PINB3)|(1<<PINB4);
 	DDRC|=(1<<PINC0)|(1<<PINC1)|(1<<PINC2); 
 	PORTC&= ~(1<<PINC0)|(1<<PINC1)|(1<<PINC2);
-    initserial(); 
+	initserial(); 
+	timer1_init(); 
+	timer1_on();
 	sei();
+	
+	
 	
     while (1) 
     {
@@ -32,13 +38,20 @@ int main(void)
     }
 }
 
+ISR(TIMER1_COMPA_vect){
+	PORTB ^= (1<<PORTB4)|(1<<PORTB3);
+	//PORTB ^= (1<<PORTB3);
+}
 
+//ISR(TIMER1_COMPB_vect){
+	//PORTB ^= (1<<PORTB3);
+//}
 
 ISR(USART_RX_vect){
 	rx= UDR0;
 	
 	
-	if (rx==''){
+	if (rx=='*'){
 		ComandoStr[RxContador]='\0';
 		RxContador=0;
 		if (strstr(ComandoStr,"a on")){PORTC|=(1<<PINC0);PORTC&= ~(1<<PINC1)|(1<<PINC2);
@@ -48,4 +61,3 @@ ISR(USART_RX_vect){
 	
 	
 }
-
